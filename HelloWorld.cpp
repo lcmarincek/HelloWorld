0c17ce241703683747752ecbb5a7b1c98e0eb670
Luis Claudio <luis.marincek@tropiconet.com>
2019-07-16 10:54:15 -0300

    Hello world!

---
+++ HelloWorld.cpp
@@ -0,0 +1,18 @@
+/*
+ * HelloWorld.cpp
+ *
+ *  Created on: 16 de jul de 2019
+ *      Author: lmarincek
+ */
+
+#include "HelloWorld.h"
+
+HelloWorld::HelloWorld() {
+	// TODO Auto-generated constructor stub
+
+}
+
+HelloWorld::~HelloWorld() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ HelloWorld.h
@@ -0,0 +1,17 @@
+/*
+ * HelloWorld.h
+ *
+ *  Created on: 16 de jul de 2019
+ *      Author: lmarincek
+ */
+
+#ifndef HELLOWORLD_H_
+#define HELLOWORLD_H_
+
+class HelloWorld {
+public:
+	HelloWorld();
+	virtual ~HelloWorld();
+};
+
+#endif /* HELLOWORLD_H_ */
